9e8ba9c098df791fcd83ef3d6bd03062ebc994d9
Farras008 <farras.alam08@gmail.com>
2022-02-24 00:39:54 +0700

    Update

--- src/konversi/Konversi.java
+++ src/konversi/Konversi.java
@@ -5,101 +5,28 @@
  */
 package konversi;
 
-import java.util.Scanner;
-
 /**
  *
  * @author Farras Alam Majid
  */
-public class Konversi {
-    static double suhuAwal;
-    static int pilihan;
-    
-    public static void main (String[] args) {
-        Suhu suhuAir = new Suhu();
-        boolean condition = false;
-        double Celcius,Fahrenheit,Reamur,Kelvin;
-        Scanner input = new Scanner(System.in);
-        System.out.println("---------------------------------------");
-        System.out.println("|        PROGRAM KONVERSI SUHU        |");
-        System.out.println("---------------------------------------");
-        System.out.println("\nSilahkan Inputkan Suhu");
-        System.out.print("Masukkan Suhu Dalam Celcius = ");
-        suhuAwal=input.nextDouble();
-        
-        while (condition!=true){
-            System.out.println("-----------------------------------");
-            System.out.println("\n\nSilahkan pilih salah satu sub menu");
-            System.out.println("---------");
-            System.out.println("1. Tampilkan Data Konversi");
-            System.out.println("2. Edit Input Suhu Awal");
-            System.out.println("3. Exit");
-            System.out.println("Pilih = ");
-            pilihan = input.nextInt();
-            
-            switch(pilihan){
-                case 1 :
-                if(suhuAwal<=0) {
-                    Celcius = suhuAir.Celcius();
-                    Fahrenheit = suhuAir.rumusFahrenheit();
-                    Reamur = suhuAir.rumusReamur();
-                    Kelvin = suhuAir.rumusKelvin();    
-                    System.out.println("Suhu Dalam Celcius\t: "+Celcius+"°C");
-                    System.out.println("Celcius ke Farenheit\t: "+Fahrenheit+"°F");
-                    System.out.println("Celcius ke Reamur\t: "+Reamur+"°R");
-                    System.out.println("Celcius ke Kelvin\t: "+Kelvin+"°K");
-                    System.out.println("Kondisi Air Beku");
-                    System.out.println("Press Enter To Continue...");
-                    new java.util.Scanner(System.in).nextLine();
-                    }
-                else if (suhuAwal<100){
-                    Celcius = suhuAir.Celcius();
-                    Fahrenheit = suhuAir.rumusFahrenheit();
-                    Reamur = suhuAir.rumusReamur();
-                    Kelvin = suhuAir.rumusKelvin();                    
-                    System.out.println("Suhu Dalam Celcius\t: "+Celcius+"°C");
-                    System.out.println("Celcius ke Farenheit\t: "+Fahrenheit+"°F");
-                    System.out.println("Celcius ke Reamur\t: "+Reamur+"°R");
-                    System.out.println("Celcius ke Kelvin\t: "+Kelvin+"K");
-                    System.out.println("Perhitungan Ini Berdasarkan Kondisi Air Normal");
-                    System.out.println("Press Enter To Continue...");
-                    new java.util.Scanner(System.in).nextLine();
-                    }
-                else if (suhuAwal>=100){
-                    Celcius = suhuAir.Celcius();
-                    Fahrenheit = suhuAir.rumusFahrenheit();
-                    Reamur = suhuAir.rumusReamur();
-                    Kelvin = suhuAir.rumusKelvin();             
-                    System.out.println("Suhu Dalam Celcius\t: "+Celcius+"°C");
-                    System.out.println("Celcius ke Farenheit\t: "+Fahrenheit+"°F");
-                    System.out.println("Celcius ke Reamur\t: "+Reamur+"°R");
-                    System.out.println("Celcius ke Kelvin\t: "+Kelvin+"K");
-                    System.out.println("Perhitungan Ini Berdasarkan Kondisi Air Mendidih"); 
-                    System.out.println("Press Enter To Continue...");
-                    new java.util.Scanner(System.in).nextLine();
-                    }
-
-                break;
-                case 2 :
-                    System.out.println("\nInput Data");
-                    System.out.println("----------");
-                    System.out.print("Masukkan suhu celcius = ");
-                    suhuAwal= input.nextDouble();
-                    System.out.println("\n\nInput berhasil...");
-                    System.out.println("Press Enter To Continue...");
-                    new java.util.Scanner(System.in).nextLine();
-                    break;
-                case 3 :
-                    condition = true ;
-                    System.out.println("Terima Kasih!");
-                    break;
-                default :
-                    System.out.println("Pilihan yang anda pilih tidak ada, silahkan ulangi!");
-                    System.out.println("Press Enter To Continue...");
-                    new java.util.Scanner(System.in).nextLine();
-            }
-        }
-        
+public class Suhu {
+    double Celcius(){
+        return(Konversi.suhuAwal);
     }
-    
+    double rumusFahrenheit(){
+        return((Konversi.suhuAwal*9)/5+32);
+    }
+    double rumusReamur(){
+        return((Konversi.suhuAwal*4)/5);
+    }
+    double rumusKelvin(){
+        return(Konversi.suhuAwal+273.15);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
 }
